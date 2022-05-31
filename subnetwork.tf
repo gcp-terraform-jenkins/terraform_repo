a87410118d5576660822e37025a92d9487292f5f
rrr-kgf <106613144+rrr-kgf@users.noreply.github.com>
2022-06-01 00:01:42 +0530

    Update subnetwork.tf

--- subnetwork.tf
+++ subnetwork.tf
@@ -1,7 +1,7 @@
 resource "google_compute_subnetwork" "subnetwork" {
  name          = "harindra-subnetwork-res01"
  network       = "harindra-vpc-res01"
- ip_cidr_range = "10.9.8.0/24" 
+ ip_cidr_range = "10.42.0.0/24" 
  depends_on = [
    google_compute_network.vpc_network
  ]
