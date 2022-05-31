b3b8400cf953ef05af389fbd0affc52d43dd0693
rrr-kgf <106613144+rrr-kgf@users.noreply.github.com>
2022-05-31 23:01:16 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "My First Project"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
