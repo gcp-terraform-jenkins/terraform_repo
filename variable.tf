405fb635ef2481876c741e0eefe2cb6649c08dd6
rrr-kgf <trainingsgcp2222@gmail.com>
2022-06-01 15:43:41 +0530

    updated

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  type = string
+  default = "sunny-shadow-350707"
+} 
+variable "reg" {
+  type = string
+  default = "us-east1"
+}
+variable "zone" {
+  type = string
+  default = "us-east1-b"
+}
+variable "subnetwork" {
+  type = string
+  default = "terraform-subnetwork"
+}
+variable "vpc_name" {
+  type = string
+  default = "terraform-vpc"
+}
